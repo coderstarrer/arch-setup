354e06dcef6c30c8088434181d9fbaacff49b5e5
V3ZE <136115292+coderstarrer@users.noreply.github.com>
2025-04-24 20:04:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -S sway swaybg rsync reflector firefox fuzzel foot pipewire pipewire-pulse pipewire-alsa pipewire-jack flatpak
+sudo pacman -S sway swaybg rsync reflector firefox fuzzel foot pipewire pipewire-pulse pipewire-alsa pipewire-jack flatpak i3blocks acpi
 systemctl --user enable --now pipewire.service pipewire.socket
 systemctl --user enable --now wireplumber.service
 systemctl --user enable --now pipewire-pulse.service pipewire-pulse.socket
@@ -14,4 +14,7 @@ cd ~/.config/sway
 cp /etc/sway/config ~/.config/sway/config
 nano config
 
+mkdir ~/.config/i3blocks
+cd ~/.config
+mv config config.save battery2 volume-pipewire LICENSE README.md time_message.sh i3blocks
 echo "Your Minimal Setup is complete, you may reboot if needed."
